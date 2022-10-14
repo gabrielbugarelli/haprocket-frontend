8fdcd6427f4934f9cf3d3cf675522bade1a066c5
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-10-14 17:13:38 -0400

    feat: config breadcrumb route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Feedback } from "../pages/Feedback";
 import { FeedbackDetails } from "../pages/FeedbackDetails";
 import { Layout } from "../pages/Layout";
+import { BreadcrumbProvider } from "../shared/hooks/useBreadcrumb";
 import { FeedbacksProvider } from "../shared/hooks/useFeedbacks";
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <FeedbacksProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Feedback />} />
-            <Route path="/feedback-detail" element={<FeedbackDetails />} />
-          </Route>
-        </Routes>
-      </FeedbacksProvider>
+      <BreadcrumbProvider>
+        <FeedbacksProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/" element={<Feedback />} />
+              <Route path="/feedback-detail" element={<FeedbackDetails />} />
+            </Route>
+          </Routes>
+        </FeedbacksProvider>
+      </BreadcrumbProvider>
     </BrowserRouter>
   )
 }
--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,8 +1,10 @@
 import { Box, Breadcrumbs, useTheme } from "@mui/material"
 import { Link } from "react-router-dom";
+import { useVerifyBreadcrumbs } from "../../hooks/useBreadcrumb";
 
 export const Header = () => {
   const theme = useTheme();
+  const { isActive } = useVerifyBreadcrumbs();
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'left' }}>
@@ -10,12 +12,18 @@ export const Header = () => {
         <Link to="/" style={{ color: theme.palette.primary.main }}>
           Feedback
         </Link>
-        <Link to="feedback-detail" style={{ color: theme.palette.primary.main }}>
-          Feedback Details
-        </Link>
+
+        {
+          isActive &&
+          <Link to="feedback-detail" style={{ color: theme.palette.primary.main }}>
+            Feedback Details
+          </Link>
+        }
       </Breadcrumbs>
 
       <p>You can send and view the feedbacks that you've received.</p>
     </Box>
-  );
+  )
+
+
 }
